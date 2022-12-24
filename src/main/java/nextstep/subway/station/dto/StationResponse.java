d44f8deb857d9ef01ddf13b02d06152dc82044f8
jylim <jylim@12cm.co.kr>
2022-12-24 14:22:36 +0900

    rollback(stationResponse): 롤백

--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -1,6 +1,5 @@
 package nextstep.subway.station.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
@@ -8,9 +7,7 @@ import java.time.LocalDateTime;
 public class StationResponse {
     private Long id;
     private String name;
-    @JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime createdDate;
-    @JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime modifiedDate;
 
     public static StationResponse of(Station station) {
